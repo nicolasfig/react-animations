559cfdb4d38eb4c1c0b644caff7193a9eaa194e6
Nicolas Figueroa <nicolasfigb@gmail.com>
2019-05-15 14:42:02 -0500

    Transition events

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,15 +3,19 @@ import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
-
 const animationTiming = {
-    enter:  400,
-    exit: 1000
-}; 
+	enter: 400,
+	exit: 1000
+};
 
 const modal = props => {
 	return (
-		<Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+		<Transition
+			in={props.show}
+			timeout={animationTiming}
+			mountOnEnter
+			unmountOnExit
+		>
 			{state => {
 				const cssClasses = [
 					"Modal",
